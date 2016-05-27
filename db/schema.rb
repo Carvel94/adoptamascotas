4bbf3f3d28d8a7c5600a4a187e477f34162c1f58
Carvel94 <carlosvel9794@gmail.com>
2016-05-27 11:29:47 -0430

    Videos de gatos con el menu

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160527142008) do
+ActiveRecord::Schema.define(version: 20160527153358) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 20160527142008) do
     t.string   "edad"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "rescatado"
   end
 
   create_table "servicios", force: :cascade do |t|
