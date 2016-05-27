d3e4c7a594a318cc152f4e68bfff434fc778ac82
Carvel94 <carlosvel9794@gmail.com>
2016-05-27 11:00:49 -0430

    Página sin bd

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'pages/home'
+
+  get 'pages/index'
+
+  resources :tipo_servicios
+  
+  root 'pages#home' #controlador pages, action home
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
